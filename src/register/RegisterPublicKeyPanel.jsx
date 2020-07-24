5f2a38598cc0d3410866880c3392f01605a4a892
Jakub Mroczek <jakub.mroczek@tomtom.com>
2020-07-24 21:39:27 +0200

    refactored register public key view

--- src/register/RegisterPublicKeyPanel.jsx
+++ src/register/RegisterPublicKeyPanel.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Form, Button } from 'react-bootstrap';
+import {
+  Form, Button, Container, Row, Col,
+} from 'react-bootstrap';
 import graphQLFetch from '../graphQLFetch.js';
 
 export default function RegisterPublicKeyPanel(props) {
@@ -13,8 +15,12 @@ export default function RegisterPublicKeyPanel(props) {
 
     const id = electionID;
     const response = await graphQLFetch(query, { id, secretToken, publicKey });
-    // TODO: Error handling
-    alert(JSON.stringify(response));
+
+    if (response) {
+      alert('Yours public key was successfully registered.')
+    } else {
+      alert('Provided secret token is invalid.')
+    }
   };
 
   const isValidPublicKey = () => {
@@ -45,28 +51,49 @@ export default function RegisterPublicKeyPanel(props) {
     await register(secretToken, publicKey);
   };
 
+  const onHomeClick = () => {
+    const { history } = props;
+    history.push('/');
+  };
+
   return (
-      <Form name="registerPublicKey">
-        <Form.Group controlId="secretToken">
-          <Form.Label>Secret token</Form.Label>
-          <Form.Control type="text" placeholder="Enter secret token" />
-          <Form.Text className="text-muted">
-            Secret token was send in the email.
-          </Form.Text>
-        </Form.Group>
-        <Form.Group controlId="publicKey">
-          <Form.Label>Public key</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Enter your Ethereum public keys"
-          />
-          <Form.Text className="text-muted">
-            Ethereum public key (look up the network name in the email).
-          </Form.Text>
-        </Form.Group>
-        <Button variant="primary" type="submit" onClick={onSubmit}>
-          Submit
-        </Button>
-      </Form>
+    <Container className="mt-3">
+      <Row className="mt-3">
+        <Col>
+          <Form name="registerPublicKey">
+            <Form.Group controlId="secretToken">
+              <Form.Label>Secret token</Form.Label>
+              <Form.Control type="text" placeholder="Enter secret token" />
+              <Form.Text className="text-muted">
+                Secret token that was send in the mail.
+              </Form.Text>
+            </Form.Group>
+            <Form.Group controlId="publicKey">
+              <Form.Label>Public key</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter your Ethereum public keys"
+              />
+              <Form.Text className="text-muted">
+                Ethereum public key.
+              </Form.Text>
+            </Form.Group>
+          </Form>
+        </Col>
+      </Row>
+      <Row className="mt-3 text-center">
+        <Col>
+          <Button variant="outline-success" type="submit" onClick={onSubmit}>
+            Submit
+          </Button>
+          {' '}
+          <Button variant="outline-success" type="submit" onClick={onHomeClick}>
+            Home
+          </Button>
+        </Col>
+      </Row>
+    </Container>
+
+
   );
 }
